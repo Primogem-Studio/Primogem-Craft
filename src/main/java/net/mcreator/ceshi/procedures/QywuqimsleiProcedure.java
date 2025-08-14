8beac9891fe8aa17c2cea38ae3e5929b098b2117
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-08-15 01:30:39 +0800

    Update

--- src/main/java/net/mcreator/ceshi/procedures/QywuqimsleiProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/QywuqimsleiProcedure.java
@@ -1,14 +1,12 @@
 package net.mcreator.ceshi.procedures;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
-
-import static net.mcreator.ceshi.PrimogemcraftMod.MODID;
 
 public class QywuqimsleiProcedure {
     private static class Type {
@@ -16,16 +14,12 @@ public class QywuqimsleiProcedure {
         public String desc;
 
         public Type(String tag, String desc) {
-            this.tag = ItemTags.create(ResourceLocation.fromNamespaceAndPath(MODID, tag));
+            this.tag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("pgc", tag));
             this.desc = desc;
         }
     }
 
-    private static final List<Type> types = List.of(
-			new Type("pgc:wuqi/danshou", "§e单手剑"),
-			new Type("pgc:wuqi/changbing", "§e长柄武器"),
-			new Type("pgc:wuqi/shuangshou", "§e双手剑")
-	);
+    private static final List<Type> types = List.of(new Type("wuqi/danshou", "§e单手剑"), new Type("wuqi/changbing", "§e长柄武器"), new Type("wuqi/shuangshou", "§e双手剑"));
 
     public static String execute(ItemStack itemstack) {
         for (var t : types) {
