2fc6926b8d9a1c71e5d31fdcdb934b4310998175
Pepper MR <zheshixbde@outlook.com>
2025-08-26 20:05:18 +0800

    完全没问题

---
+++ src/main/java/net/mcreator/ceshi/procedures/QywuqihszProcedure.java
@@ -0,0 +1,54 @@
+package net.mcreator.ceshi.procedures;
+
+import net.hackermdch.pgc.CustomAPI;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public class QywuqihszProcedure {
+    private static class WQType {
+        private final TagKey<Item> tag;
+        private final double cheng;
+        private final Holder<Attribute> z;
+
+
+        public WQType(String tag, double cheng, Holder<Attribute> z) {
+            this.tag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("pgc", tag));
+            this.cheng = cheng;
+            this.z = z;
+        }
+
+        public boolean ss(ItemStack stack, double c) {
+            if (stack.is(tag)) {
+                var attr = CustomAPI.getAttributes(stack);
+                attr.add(z, "djjc", c * cheng, AttributeModifier.Operation.ADD_VALUE, EquipmentSlotGroup.MAINHAND);
+                attr.apply();
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static final List<WQType> types = List.of(
+            new WQType("wuqi/danshou", 1f, Attributes.ATTACK_DAMAGE),
+            new WQType("wuqi/changbing", 1.3f, Attributes.ATTACK_DAMAGE),
+            new WQType("wuqi/gongju", 0.7f, Attributes.MINING_EFFICIENCY),
+            new WQType("wuqi", 1, Attributes.ATTACK_DAMAGE)
+    );
+
+    public static boolean execute(ItemStack itemstack, double c) {
+        for (var t : types) {
+            if (t.ss(itemstack, c)) return true;
+        }
+        return false;
+    }
+}
--- src/main/java/net/mcreator/ceshi/procedures/WuqishuaxinProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/WuqishuaxinProcedure.java
@@ -3,20 +3,11 @@ package net.mcreator.ceshi.procedures;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.core.Holder;
 
 import net.mcreator.ceshi.network.PrimogemcraftModVariables;
 import net.mcreator.ceshi.init.PrimogemcraftModGameRules;
-
-import net.hackermdch.pgc.CustomAPI;
 
 public class WuqishuaxinProcedure {
 	public static boolean execute(LevelAccessor world, Entity entity, ItemStack item) {
@@ -42,41 +33,15 @@ public class WuqishuaxinProcedure {
 			b = a1 * a11 * (0.02 + a12 * 1.5);
 			c = c + b;
 			c = c + (a - a2) * a11 * (0.03 + a12 * 2);
-			return WuqishuaxinProcedure.wuqishixian(stack, c);
+			return QywuqihszProcedure.execute(stack, c);
 		} else if (a >= a1) {
 			b = a1 * a11 * (0.01 + a12);
 			c = c + b;
 			c = c + (a - a1) * a11 * (0.02 + a12 * 1.5);
-			return WuqishuaxinProcedure.wuqishixian(stack, c);
+			return QywuqihszProcedure.execute(stack, c);
 		} else if (a < a1) {
 			c = c + a * a11 * (0.01 + a12);
-			return WuqishuaxinProcedure.wuqishixian(stack, c);
-		}
-		return false;
-	}
-
-	public static boolean wuqishixian(ItemStack stack, double c) {
-		var z = Attributes.ATTACK_DAMAGE;
-		boolean o1 = false;
-		if (NBTwuqi(stack, "pgc:wuqi/danshou", 1f, c, z)) {
-			o1 = true;
-		} else if (NBTwuqi(stack, "pgc:wuqi/changbing", 1.3f, c, z)) {
-			o1 = true;
-		} else if (NBTwuqi(stack, "pgc:wuqi/gongju", 0.7f, c, Attributes.MINING_EFFICIENCY)) {
-			o1 = true;
-		} else if (NBTwuqi(stack, "pgc:wuqi", 1, c, z)) {
-			o1 = true;
-		}
-		//!!! 暂时先这样，之后再改 !!!!
-		return o1;
-	}
-
-	public static boolean NBTwuqi(ItemStack stack, String tag, float cheng, double c, Holder<Attribute> z) {
-		if (stack.is(ItemTags.create(ResourceLocation.parse(tag)))) {
-			var attr = CustomAPI.getAttributes(stack);
-			attr.add(z, "djjc", c * cheng, AttributeModifier.Operation.ADD_VALUE, EquipmentSlotGroup.MAINHAND);
-			attr.apply();
-			return true;
+			return QywuqihszProcedure.execute(stack, c);
 		}
 		return false;
 	}
