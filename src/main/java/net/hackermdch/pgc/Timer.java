1a634ac94f6b2f0e08b97f98c9477e1f0daba9ea
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-08-27 22:34:25 +0800

    Add timer

---
+++ src/main/java/net/hackermdch/pgc/Timer.java
@@ -0,0 +1,47 @@
+package net.hackermdch.pgc;
+
+import net.minecraft.world.entity.Entity;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static net.mcreator.ceshi.PrimogemcraftMod.MODID;
+
+@EventBusSubscriber(modid = MODID)
+public class Timer {
+    private static final Map<Entity, Map<String, Timer>> map = new HashMap<>();
+    private int tick;
+
+    private Timer(int tick) {
+        this.tick = tick;
+    }
+
+    public static void set(Entity entity, String name, int tick) {
+        if (!map.containsKey(entity)) map.put(entity, new HashMap<>());
+        var timers = map.get(entity);
+        timers.put(name, new Timer(tick));
+    }
+
+    public static boolean isDone(Entity entity, String name) {
+        if (!map.containsKey(entity)) return true;
+        var timers = map.get(entity);
+        if (!timers.containsKey(name)) return true;
+        return timers.get(name).tick <= 0;
+    }
+
+    @SubscribeEvent
+    private static void onTick(ServerTickEvent.Post event) {
+        var actions = new ArrayList<Runnable>();
+        for (var entry : map.values()) {
+            for (var timer : entry.entrySet()) {
+                timer.getValue().tick--;
+                if (timer.getValue().tick <= 0) actions.add(() -> entry.remove(timer.getKey()));
+            }
+        }
+        actions.forEach(Runnable::run);
+    }
+}
