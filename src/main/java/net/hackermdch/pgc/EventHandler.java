82c27fcfa590d59bb17a5f4e01a400e451922ad0
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-08-27 21:02:47 +0800

    Update

--- src/main/java/net/hackermdch/pgc/EventHandler.java
+++ src/main/java/net/hackermdch/pgc/EventHandler.java
@@ -5,16 +5,23 @@ import net.hackermdch.pgc.network.WishInfoConfiguration;
 import net.hackermdch.pgc.network.WishInfoPacket;
 import net.mcreator.ceshi.CustomBarRegister;
 import net.mcreator.ceshi.GenshinCraftLinkage;
+import net.mcreator.ceshi.init.PrimogemcraftModItems;
+import net.mcreator.ceshi.init.PrimogemcraftModTabs;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterItemDecorationsEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
 import net.neoforged.neoforge.network.event.RegisterConfigurationTasksEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.registries.RegisterEvent;
+
+import java.util.function.BiConsumer;
 
 import static net.mcreator.ceshi.PrimogemcraftMod.MODID;
 
@@ -53,6 +60,14 @@ public class EventHandler {
     private static void register(RegisterConfigurationTasksEvent event) {
         if (ModList.get().isLoaded("roughlyenoughitems")) {
             event.register(new WishInfoConfiguration(event.getListener()));
+        }
+    }
+
+    @SubscribeEvent
+    private static void onBuildTabContents(BuildCreativeModeTabContentsEvent event) {
+        BiConsumer<ItemStack, ItemStack> insert = (point, stack) -> event.insertAfter(point, stack, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        if (event.getTabKey() == PrimogemcraftModTabs.PRIMOGEMCRAFT_EQUIPMENT.getKey()) {
+            insert.accept(PrimogemcraftModItems.TCZDG.toStack(), PrimogemcraftModItems.HPY.toStack());
         }
     }
 }
--- src/main/java/net/mcreator/ceshi/item/Hpy.java
+++ src/main/java/net/mcreator/ceshi/item/Hpy.java
@@ -1,62 +1,53 @@
 package net.mcreator.ceshi.item;
 
-import net.mcreator.ceshi.init.PrimogemcraftModItems;
-import net.mcreator.ceshi.procedures.*;
+import net.mcreator.ceshi.procedures.HpymsProcedure;
+import net.mcreator.ceshi.procedures.Hpysx0Procedure;
+import net.mcreator.ceshi.procedures.JlqhewaiProcedure;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.MaceItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
 import java.util.List;
 
+import static net.minecraft.world.entity.ai.attributes.AttributeModifier.Operation.ADD_VALUE;
+
 public class Hpy extends MaceItem {
-    private static final Tier TOOL_TIER = new Tier() {
-        @Override
-        public int getUses() {
-            return 3000;
-        }
-
-        @Override
-        public float getSpeed() {
-            return 4f;
-        }
-
-        @Override
-        public float getAttackDamageBonus() {
-            return 0;
-        }
-
-        @Override
-        public TagKey<Block> getIncorrectBlocksForDrops() {
-            return BlockTags.INCORRECT_FOR_WOODEN_TOOL;
-        }
-
-        @Override
-        public int getEnchantmentValue() {
-            return 20;
-        }
-
-        @Override
-        public Ingredient getRepairIngredient() {
-            return Ingredient.of(new ItemStack(PrimogemcraftModItems.JLLIANG.get()));
-        }
-    };
     public Hpy() {
-        super(new Item.Properties().attributes(SwordItem.createAttributes(TOOL_TIER, 9f, -2f)).fireResistant());
+        super(new Properties().attributes(createCustomAttributes()).stacksTo(1).durability(100).fireResistant());
     }
+
+    private static ItemAttributeModifiers createCustomAttributes() {
+        return ItemAttributeModifiers.builder()
+                .add(
+                        Attributes.ATTACK_DAMAGE,
+                        new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 9, ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND
+                )
+                .add(
+                        Attributes.ATTACK_SPEED,
+                        new AttributeModifier(BASE_ATTACK_SPEED_ID, -2, ADD_VALUE),
+                        EquipmentSlotGroup.MAINHAND
+                )
+                .build();
+    }
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-        InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+        var ar = super.use(world, entity, hand);
         HpymsProcedure.execute(entity, ar.getObject());
         return ar;
     }
@@ -65,20 +56,15 @@ public class Hpy extends MaceItem {
     @OnlyIn(Dist.CLIENT)
     public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
         super.appendHoverText(itemstack, context, list, flag);
-        Entity entity = itemstack.getEntityRepresentation() != null ? itemstack.getEntityRepresentation() : Minecraft.getInstance().player;
-        String hoverText = HpymsProcedure.execute(entity, itemstack);
-        if (hoverText != null) {
-            for (String line : hoverText.split("\n")) {
-                list.add(Component.literal(line));
-            }
-        }
+        var entity = itemstack.getEntityRepresentation() != null ? itemstack.getEntityRepresentation() : Minecraft.getInstance().player;
+        var hoverText = HpymsProcedure.execute(entity, itemstack);
+        for (var line : hoverText.split("\n")) list.add(Component.literal(line));
     }
 
     @Override
     public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(itemstack, world, entity, slot, selected);
-        if (selected)
-            Hpysx0Procedure.execute(entity, itemstack);
+        if (selected) Hpysx0Procedure.execute(entity, itemstack);
         JlqhewaiProcedure.execute(entity, itemstack);
     }
 }
