e913d29a6f6c8471d515e1384da189082ba9cad5
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-08-28 23:28:48 +0800

    Update

--- src/main/java/net/mcreator/ceshi/procedures/Hpysx0Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Hpysx0Procedure.java
@@ -10,20 +10,24 @@ public class Hpysx0Procedure {
 	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
+		String s1 = "";
 		double a = 0;
 		double n = 0;
-		String s1 = "";
+		double b = 0;
 		if (!world.isClientSide()) {
 			a = HSjinglianupProcedure.execute(entity, itemstack);
 			n = itemstack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("hpy_bd_j");
 			s1 = "hpy_e";
-			if (n < 2 + (a > 3 ? a - 3 : 1) && net.hackermdch.pgc.Timer.isDone(entity, s1)) {
+			b = a > 3 ? a - 1 : 3;
+			if (n < b && net.hackermdch.pgc.Timer.isDone(entity, s1)) {
 				{
 					final String _tagName = "hpy_bd_j";
 					final double _tagValue = (n + 1);
 					CustomData.update(DataComponents.CUSTOM_DATA, itemstack, tag -> tag.putDouble(_tagName, _tagValue));
 				}
-				net.hackermdch.pgc.Timer.set(entity, s1, (int) (160 - 20 * a));
+				if (n < b - 1) {
+					net.hackermdch.pgc.Timer.set(entity, s1, (int) (160 - 20 * a));
+				}
 			}
 		}
 	}
