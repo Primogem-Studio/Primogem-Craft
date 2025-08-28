8994b7574e6cf26110e9324b436101e346ef05f2
hackermdch <58121864+hackermdch@users.noreply.github.com>
2025-08-29 00:22:23 +0800

    Update

--- src/main/java/net/hackermdch/pgc/command/Test.java
+++ src/main/java/net/hackermdch/pgc/command/Test.java
@@ -1,6 +1,8 @@
 package net.hackermdch.pgc.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.mcreator.ceshi.procedures.WuqishuaxinProcedure;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.component.DataComponents;
@@ -12,23 +14,26 @@ import net.minecraft.world.item.component.CustomData;
 public class Test {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         var tag = ItemTags.create(ResourceLocation.fromNamespaceAndPath("pgc", "wuqi"));
-        dispatcher.register(Commands.literal("pgc-test").requires(c -> c.hasPermission(2)).then(Commands.literal("1").executes(c -> {
+        dispatcher.register(Commands.literal("pgc-test").requires(c -> c.hasPermission(2)).then(Commands.literal("set-weapon").then(Commands.argument("level", IntegerArgumentType.integer()).then(Commands.argument("qua", IntegerArgumentType.integer()).executes(c -> {
             var p = c.getSource().getPlayer();
             if (p != null) {
                 var it = p.getMainHandItem();
                 if (it.is(tag)) {
+                    var level = IntegerArgumentType.getInteger(c, "level");
+                    var qua = IntegerArgumentType.getInteger(c, "qua");
                     CustomData.update(DataComponents.CUSTOM_DATA, it, t -> {
-                        t.putDouble("deng_ji", 89);
-                        t.putDouble("jing_lian", 9);
+                        t.putDouble("deng_ji", level - 1);
+                        t.putDouble("jing_lian", qua - 1);
                     });
-                    c.getSource().sendSuccess(() -> Component.literal("已强化至最大"), true);
+                    WuqishuaxinProcedure.execute(p.level(), p, it);
+                    c.getSource().sendSuccess(() -> Component.literal("已设置至：精炼" + qua + "等级" + level), true);
                     return 0;
                 }
-                c.getSource().sendFailure(Component.literal("无法强化此物品"));
+                c.getSource().sendFailure(Component.literal("无法设置此物品"));
                 return 1;
             }
             c.getSource().sendFailure(Component.literal("无效的命令执行者"));
             return 2;
-        })));
+        })))));
     }
 }
