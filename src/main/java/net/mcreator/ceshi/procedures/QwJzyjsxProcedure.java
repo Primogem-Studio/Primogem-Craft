e1d4125cb40b84c07dd7200c51c19c356a9af564
Pepper MR <zheshixbde@outlook.com>
2025-10-05 18:09:16 +0800

    小问题

--- src/main/java/net/mcreator/ceshi/procedures/QwJzyjsxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/QwJzyjsxProcedure.java
@@ -2,48 +2,28 @@ package net.mcreator.ceshi.procedures;
 
 import org.checkerframework.checker.units.qual.h;
 
-import net.neoforged.neoforge.items.IItemHandlerModifiable;
-import net.neoforged.neoforge.capabilities.Capabilities;
-
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.resources.ResourceLocation;
 
 import net.mcreator.ceshi.init.PrimogemcraftModAttributes;
-
-import net.hackermdch.pgc.CustomAPI;
+import net.mcreator.ceshi.PrimogemcraftMod;
 
 public class QwJzyjsxProcedure {
 	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		double a = 0;
-		double z = 0;
 		double h = 0;
 		ItemStack stack = ItemStack.EMPTY;
 		if (!world.isClientSide()) {
-			if (net.hackermdch.pgc.Timer.isDone(entity, "jzyj")) {
-				net.hackermdch.pgc.Timer.set(entity, "jzyj", 40);
+			stack = itemstack;
+			if (EwaishhsProcedure.execute(entity, "jzyj")) {
 				h = entity instanceof LivingEntity _livEnt ? _livEnt.getMaxHealth() : -1;
-				if (entity.getCapability(Capabilities.ItemHandler.ENTITY, null) instanceof IItemHandlerModifiable _modHandlerIter) {
-					for (int _idx = 0; _idx < _modHandlerIter.getSlots(); _idx++) {
-						ItemStack itemstackiterator = _modHandlerIter.getStackInSlot(_idx).copy();
-						if (itemstackiterator.is(ItemTags.create(ResourceLocation.parse("c:curio/bad")))) {
-							a = a + itemstackiterator.getCount();
-						}
-					}
-				}
-				z = Math.min(h * 0.3, h * 0.003 * a);
-				stack = itemstack;
-				var attr = CustomAPI.getAttributes(stack);
-				attr.add(PrimogemcraftModAttributes.EWAI_SHANGHAI_MF, "jzyj", z, AttributeModifier.Operation.ADD_VALUE, EquipmentSlotGroup.ANY);
-				attr.apply();
+				EwaishhsProcedure.sl(Math.min(h * 0.3, h * 0.003 * EwaishhsProcedure.obd(entity)), "jzyj", itemstack);
 			}
+			PrimogemcraftMod.LOGGER.info(
+					entity instanceof LivingEntity _livingEntity3 && _livingEntity3.getAttributes().hasAttribute(PrimogemcraftModAttributes.EWAI_SHANGHAI_MF) ? _livingEntity3.getAttribute(PrimogemcraftModAttributes.EWAI_SHANGHAI_MF).getValue() : 0);
 		}
 	}
 }
