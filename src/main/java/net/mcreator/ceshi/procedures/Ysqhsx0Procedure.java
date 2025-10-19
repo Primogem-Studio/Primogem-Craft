371f0dd3460656620559017c32662ff8f154a807
Pepper MR <zheshixbde@outlook.com>
2025-10-20 02:40:06 +0800

    AI真是太他妈好用了

--- src/main/java/net/mcreator/ceshi/procedures/Ysqhsx0Procedure.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.ceshi.procedures;
-
-import net.minecraft.world.item.ItemStack;
-
-public class Ysqhsx0Procedure {
-	public static void execute(ItemStack item) {
-		if ()
-	}
-}
