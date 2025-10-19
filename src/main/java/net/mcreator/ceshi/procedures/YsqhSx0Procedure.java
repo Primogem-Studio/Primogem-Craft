f0b343a205fafcd0d6bc16eedcca1dbe212326de
Pepper MR <zheshixbde@outlook.com>
2025-10-20 03:21:34 +0800

    优化下刻触发

--- src/main/java/net/mcreator/ceshi/procedures/YsqhSx0Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/YsqhSx0Procedure.java
@@ -1,41 +1,39 @@
 package net.mcreator.ceshi.procedures;
 
-import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.bus.api.Event;
-
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.Event;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 import javax.annotation.Nullable;
 
+import static net.minecraft.world.entity.EquipmentSlot.*;
+
 @EventBusSubscriber
 public class YsqhSx0Procedure {
-	@SubscribeEvent
-	public static void onPlayerTick(PlayerTickEvent.Post event) {
-		execute(event, event.getEntity());
-	}
+    @SubscribeEvent
+    public static void onPlayerTick(PlayerTickEvent.Post event) {
+        execute(event, event.getEntity());
+    }
 
-	public static void execute(Entity entity) {
-		execute(null, entity);
-	}
+    public static void execute(Entity entity) {
+        execute(null, entity);
+    }
 
-	private static void execute(@Nullable Event event, Entity entity) {
-		if (entity == null)
-			return;
-		double yys = 0;
-		double mmb = 0;
-		ItemStack i1 = ItemStack.EMPTY;
-		ItemStack i2 = ItemStack.EMPTY;
-		ItemStack i3 = ItemStack.EMPTY;
-		ItemStack i4 = ItemStack.EMPTY;
-		i1 = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
-		i2 = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.LEGS) : ItemStack.EMPTY);
-		i3 = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.CHEST) : ItemStack.EMPTY);
-		i4 = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.HEAD) : ItemStack.EMPTY);
-		YsqhsxProcedure.execute(i1, i2, i3, i4);
-	}
+    private static void execute(@Nullable Event event, Entity entity) {
+        if (entity == null)
+            return;
+        if (net.hackermdch.pgc.Timer.isDone(entity, "ysqh")) {
+            net.hackermdch.pgc.Timer.set(entity,"ysqh",40);
+            YsqhsxProcedure.execute(Bw(FEET, entity), Bw(LEGS, entity), Bw(CHEST, entity), Bw(HEAD, entity));
+        }
+    }
+
+    public static ItemStack Bw(EquipmentSlot bw, Entity entity) {
+        return (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(bw) : ItemStack.EMPTY);
+    }
 }
