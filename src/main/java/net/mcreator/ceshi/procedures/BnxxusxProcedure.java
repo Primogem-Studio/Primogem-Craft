6d92f98f9ac5b47be467f7414ad32bd20c211ad5
Pepper MR <zheshixbde@outlook.com>
2025-10-21 03:29:04 +0800

    t

--- src/main/java/net/mcreator/ceshi/procedures/BnxxusxProcedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/BnxxusxProcedure.java
@@ -7,6 +7,7 @@ import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
 
 import net.mcreator.ceshi.init.PrimogemcraftModMobEffects;
+import net.mcreator.ceshi.PrimogemcraftMod;
 
 public class BnxxusxProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
@@ -15,23 +16,26 @@ public class BnxxusxProcedure {
 		double a = 0;
 		double b = 0;
 		double hp = 0;
-		a = entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(PrimogemcraftModMobEffects.BNXXU) ? _livEnt.getEffect(PrimogemcraftModMobEffects.BNXXU).getAmplifier() : 0;
-		b = entity.getPersistentData().getDouble("bnxxu");
-		hp = entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1;
-		if (entity.isInPowderSnow) {
-			entity.getPersistentData().putDouble("bnxxu", (b + 1));
-			if (net.hackermdch.pgc.Timer.isDone(entity, "bnxxu")) {
-				net.hackermdch.pgc.Timer.set(entity, "bnxxu", (int) (40 - a * 5));
-				hp(world, entity, a);
-			}
-			entity.setTicksFrozen(1);
-		} else if (b > 0 && net.hackermdch.pgc.Timer.isDone(entity, "bnxxu_h")) {
-			net.hackermdch.pgc.Timer.set(entity, "bnxxu_h", 200);
-			if (a >= 4) {
-				entity.hurt(new DamageSource(world.holderOrThrow(DamageTypes.FREEZE)), (float) Math.min(hp - 1, b / 20));
+		if (!world.isClientSide()) {
+			a = entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(PrimogemcraftModMobEffects.BNXXU) ? _livEnt.getEffect(PrimogemcraftModMobEffects.BNXXU).getAmplifier() : 0;
+			PrimogemcraftMod.LOGGER.info(entity instanceof LivingEntity _livEnt && _livEnt.hasEffect(PrimogemcraftModMobEffects.BNXXU) ? _livEnt.getEffect(PrimogemcraftModMobEffects.BNXXU).getAmplifier() : 0);
+			b = entity.getPersistentData().getDouble("bnxxu");
+			hp = entity instanceof LivingEntity _livEnt ? _livEnt.getHealth() : -1;
+			if (entity.getTicksFrozen() > 1) {
+				entity.getPersistentData().putDouble("bnxxu", (b + 1));
+				if (net.hackermdch.pgc.Timer.isDone(entity, "bnxxu")) {
+					net.hackermdch.pgc.Timer.set(entity, "bnxxu", (int) (40 - a * 5));
+					hp(world, entity, a);
+				}
+				entity.setTicksFrozen(1);
+			} else if (b > 0 && net.hackermdch.pgc.Timer.isDone(entity, "bnxxu_h")) {
+				net.hackermdch.pgc.Timer.set(entity, "bnxxu_h", 200);
+				if (a >= 4) {
+					entity.hurt(new DamageSource(world.holderOrThrow(DamageTypes.FREEZE)), (float) Math.min(hp - 1, b / 20));
+				} else {
+					hp(world, entity, ((b / 40) * a) / 20);
+				}
 				entity.getPersistentData().putDouble("bnxxu", 0);
-			} else {
-				hp(world, entity, ((b / 40) * a) / 20);
 			}
 		}
 	}
