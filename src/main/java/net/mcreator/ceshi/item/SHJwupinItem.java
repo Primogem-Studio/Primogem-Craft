907c8a4cb9b66de1e6e8fba74c7269df0e16259e
Pepper MR <zheshixbde@outlook.com>
2025-11-23 13:17:09 +0800

    配方

--- src/main/java/net/mcreator/ceshi/item/SHJwupinItem.java
+++ src/main/java/net/mcreator/ceshi/item/SHJwupinItem.java
@@ -1,10 +1,21 @@
 package net.mcreator.ceshi.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+
+import net.mcreator.ceshi.procedures.Shijian_it_sxProcedure;
 
 public class SHJwupinItem extends Item {
 	public SHJwupinItem() {
 		super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.RARE));
+	}
+
+	@Override
+	public void onCraftedBy(ItemStack itemstack, Level world, Player entity) {
+		super.onCraftedBy(itemstack, world, entity);
+		Shijian_it_sxProcedure.execute(itemstack);
 	}
 }
