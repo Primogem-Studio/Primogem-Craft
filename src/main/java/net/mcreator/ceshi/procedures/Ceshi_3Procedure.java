3cc1880a5c31fc8b6e17edcf5fbf4ff86e666ce8
Pepper MR <zheshixbde@outlook.com>
2025-12-01 18:55:43 +0800

    t

--- src/main/java/net/mcreator/ceshi/procedures/Ceshi_3Procedure.java
+++ src/main/java/net/mcreator/ceshi/procedures/Ceshi_3Procedure.java
@@ -1,28 +1,21 @@
 package net.mcreator.ceshi.procedures;
 
-import net.mcreator.ceshi.event.Event;
-import net.mcreator.ceshi.init.PrimogemcraftModItems;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.Entity;
 
 public class Ceshi_3Procedure {
-    public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
-        if (entity == null)
-            return;
-        double ceshi_01 = 0;
-        int a = 0;
-        ItemStack stack = ItemStack.EMPTY;
-        Entity e = null;
-        boolean o1 = false;
-        boolean w = false;
-        stack = itemstack;
-        e = entity;
-        o1 = world.isClientSide();
-
-        var i = new ItemStack(PrimogemcraftModItems.YUZHOUSUIPIAN.get());
-
-        Event event = new Event(a, e, world);
-        if (event.use(event.comItem(i, 20))) event.openEnchantGui(3);
-    }
+	public static void execute(LevelAccessor world, Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		double ceshi_01 = 0;
+		double a = 0;
+		ItemStack stack = ItemStack.EMPTY;
+		Entity e = null;
+		boolean o1 = false;
+		boolean w = false;
+		stack = itemstack;
+		e = entity;
+		o1 = world.isClientSide();
+	}
 }
