23b2f7c9848d5fa73ed9dd7268efdd6b9613d876
Pepper MR <zheshixbde@outlook.com>
2026-01-03 06:35:05 +0800

    框架差不多了，给效果全扔进去就完事
    t

--- src/main/java/net/per/curio/implement/Lottery.java
+++ src/main/java/net/per/curio/implement/Lottery.java
@@ -1,40 +1,78 @@
 package net.per.curio.implement;
 
+import net.mcreator.ceshi.init.PrimogemcraftModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.per.curio.CurioEffectPGC;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@EventBusSubscriber
 public class Lottery {
-    public static void register(IEventBus eventBus) {
-        eventBus.register(Lottery.class);
-    }
 
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
-        System.out.println("aaa");
         Player player = event.getPlayer();
         if (player == null) return;
-        ItemStack curioItem = getCurioItemFromPlayer(player);
+        ItemStack curioItem = player instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY;
         if (curioItem.isEmpty()) return;
+        if (!curioItem.is(ItemTags.create(ResourceLocation.parse("c:curio"))))return;
         CurioEffectPGC.Processor processor = new CurioEffectPGC.Processor(event.getLevel(), player, curioItem);
-
-
-        processor.lottery(0.5,
-                () -> {
-                    processor.spawnTable("minecraft:chests/simple_dungeon");
-                    processor.announce("§a饰品效果触发：发现了宝藏！");
-                },
-                () -> {
-                    processor.announce("§c饰品效果失败，并发生了损耗。");
-                }
-        );
+        effect(processor);
     }
 
-    private static ItemStack getCurioItemFromPlayer(Player player) {
-        return player instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY;
+    private static Map<Item, Consumer<CurioEffectPGC.Processor>> effectMap;
+
+    private static Map<Item, Consumer<CurioEffectPGC.Processor>> getEffectMap() {
+        if (effectMap == null) {
+            effectMap = new HashMap<>();
+            effectMap.put(PrimogemcraftModItems.QWLZDLT.get(), cp ->
+                    cp.lottery(0.5,
+                            () -> {
+                                cp.spawnTable("minecraft:chests/desert_pyramid");
+                                cp.announce("§d遗音河大乐透触发！");
+                            },
+                            () -> cp.announce("§5遗音河能量消散...")
+                    ));
+            effectMap.put(PrimogemcraftModItems.QWXYDLT.get(), cp ->
+                    cp.lottery(0.5,
+                            () -> {
+                                cp.spawnTable("minecraft:chests/desert_pyramid");
+                                cp.announce("§d遗音河大乐透触发！");
+                            },
+                            () -> cp.announce("§5遗音河能量消散...")
+                    ));
+            effectMap.put(PrimogemcraftModItems.QWHYDLT.get(), cp ->
+                    cp.lottery(0.5,
+                            () -> {
+                                cp.spawnTable("minecraft:chests/desert_pyramid");
+                                cp.announce("§d遗音河大乐透触发！");
+                            },
+                            () -> cp.announce("§5遗音河能量消散...")
+                    ));
+            effectMap.put(PrimogemcraftModItems.YIYINHEDALETOU.get(), cp ->
+                    cp.lottery(0.5,
+                            () -> {
+                                cp.spawnTable("minecraft:chests/desert_pyramid");
+                                cp.announce("§d遗音河大乐透触发！");
+                            },
+                            () -> cp.announce("§5遗音河能量消散...")
+                    ));
+        }
+        return effectMap;
+    }
+
+    private static void effect(CurioEffectPGC.Processor cp) {
+        Consumer<CurioEffectPGC.Processor> effect = getEffectMap().get(cp.getCurio().getItem());
+        if (effect != null) effect.accept(cp);
     }
 }
