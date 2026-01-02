b362fee375e3d1b15642bf87469d736d25158909
Pepper MR <zheshixbde@outlook.com>
2026-01-02 20:10:26 +0800

    以后再说吧~

--- src/main/java/net/per/curio/CurioEffectPGC.java
+++ src/main/java/net/per/curio/CurioEffectPGC.java
@@ -1,14 +1,21 @@
 package net.per.curio;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 
 import java.util.HashSet;
+import java.util.function.Supplier;
 
 public class CurioEffectPGC {
     public static class Processor {
@@ -18,6 +25,10 @@ public class CurioEffectPGC {
         public Processor(LevelAccessor world, Entity entity) {
             this.world = world;
             this.player = (Player) entity;
+        }
+
+        public boolean getRandomResult(double value) {
+            return !world.isClientSide() ? Math.random() < value : false;
         }
 
         public double curioDice(boolean remove, ItemPredicate condition) {
@@ -37,6 +48,27 @@ public class CurioEffectPGC {
         @FunctionalInterface
         public interface ItemPredicate {
             boolean test(ItemStack itemstack);
+        }
+        
+        /**
+         * 工具
+        */
+        public void announceCurioBroken(ItemStack item){
+            announce(item.getDisplayName().getString()+"§c已损坏！");
+            playAudio();
+        }
+
+        public void announce(String s){
+            if (!player.level().isClientSide())
+                player.displayClientMessage(Component.literal(s), false);
+        }
+
+        private void playAudio() {
+            if (world instanceof Level _level) {
+                if (!_level.isClientSide()) {
+                    _level.playSound(null, BlockPos.containing(player.getX(), player.getY(), player.getZ()), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("entity.firework_rocket.launch")), SoundSource.PLAYERS, 1, 1);
+                }
+            }
         }
     }
 
